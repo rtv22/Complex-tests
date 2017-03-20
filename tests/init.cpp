3a1f5bd7497af0941cd1e8269b1c177dd08f8353
rtv22 <rtvcojigor@gmail.com>
2017-03-20 21:39:55 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 SCENARIO("coplex init", "[init]"){
-  Complex num_1;
+  complex num_1;
   REQUARE (num_1.r() == 0);
   REQUARE (num_1.i() == 0);
 }
@@ -13,7 +13,7 @@ SCENARIO("coplex init", "[init]"){
 SCENARIO("params init", "[init with params]") 
 {
 	int init = 2;
-	Complex num_1(init);
+	complex num_1(init, init);
 	REQUIRE(num_1.r() == 2);
 	REQUIRE(num_1.i() == 2);
 }
