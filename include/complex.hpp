dbfe7b8f0b00c9e15a5e3a899740c1422ccc8ad4
rtv22 <rtvcojigor@gmail.com>
2017-03-20 19:22:58 +0300

    Update complex.hpp

--- include/complex.hpp
+++ include/complex.hpp
@@ -1,1 +1,32 @@
+#include <iostream>
 
+using namespace std;
+
+class complex
+{
+private:
+	double real;
+	double imaginary;
+public:
+	complex(double a, double b)
+	{
+		real = a;
+		imaginary = b;
+	}
+	complex();
+	void set();
+	void print(std::ostream&) const;
+	complex sum(complex) const;
+	complex sub(complex) const;
+	complex mult(int) const;
+	complex div(int) const;
+	complex(const complex&);
+	complex operator*(complex);
+	complex operator/(complex );
+	bool operator==(complex);
+	complex operator=(complex);
+	complex operator+=(complex);
+	complex operator-=(complex);
+	complex operator*=(complex);
+	complex operator/=(complex);
+};
